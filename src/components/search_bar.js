6bd3e1a46251325e67372b23e0a572059bc1d13f
Mauro Bono <bono.mauro@gmail.com>
2018-03-27 17:10:08 +0200

    setup main App, SearchBar, VideoList and VideoListItems component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-    return <input placeholder="search..."/>;
+//a plain JS Object
+//this is a Class-based Component
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        //set the initial state of the component
+        this.state = { term: '' };
+    }
+    render() {
+        return (
+            <div>
+                <input
+                    value={ this.state.term }
+                    onChange={ event => this.setState({ term: event.target.value }) }
+                />
+            </div>
+        );
+    }
+
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import API_KEY from './config';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 
-const App = () => {
-    return (
-        <div>
-        <SearchBar />
-        </div>
-    );
-};
+//App Component
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+            this.setState({ videos }); //{ videos: videos } before ES6
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                {/* videos is a react prop */}
+                <VideoList videos={ this.state.videos }/>
+            </div>
+        );
+    }
+}
+
 
 ReactDOM.render(<App />, document.querySelector('.container'));
