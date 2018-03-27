74665b917bc3f72a83b1d60d8611806f025ff38d
Mauro <bono.mauro@gmail.com>
2018-03-27 23:07:03 +0200

    add some components style, add select video functionality and other methods to the components

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -2,7 +2,15 @@ import React, { Component } from 'react';
 import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
-    const VideoItems = props.videos.map(video => <VideoListItem key={video.etag} video={video} />);
+    const VideoItems = props.videos.map((video) => {
+        return (
+        <VideoListItem
+            onVideoSelect={props.onVideoSelect} 
+            key={video.etag} 
+            video={video} 
+        />
+        );
+    });
 
     return (
         <ul className="col-md-4 list-group">
--- src/index.js
+++ src/index.js
@@ -4,16 +4,23 @@ import YTSearch from 'youtube-api-search';
 import API_KEY from './config';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 
 //App Component
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = { videos: [] };
+        this.state = { 
+            videos: [],
+            selectedVideo: null 
+        };
 
         YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
-            this.setState({ videos }); //{ videos: videos } before ES6
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0] 
+            });
         });
     }
 
@@ -21,8 +28,12 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoDetail video={this.state.selectedVideo} />
                 {/* videos is a react prop */}
-                <VideoList videos={ this.state.videos }/>
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={ this.state.videos }
+                />
             </div>
         );
     }
